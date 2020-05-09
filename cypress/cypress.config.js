afd6dfb3c216dcbe0feae66c0da16258ed94585c
Chunpeng Huo <huocp@me.com>
2020-05-09 13:22:41 +1000

    test: cleanup

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -6,12 +6,6 @@ const HOST = CLIOptions.getFlagValue('host') || aureliaConfig.platform.host;
 module.exports = {
   config: {
     baseUrl: `http://${HOST}:${PORT}`,
-    video: false/* @if ci */,
-    defaultCommandTimeout: 40000,
-    execTimeout: 600000,
-    taskTimeout: 600000,
-    pageLoadTimeout: 600000,
-    requestTimeout: 50000,
-    responseTimeout: 300000/* @endif */
+    video: false
   }
 };
--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-const questions = [
+module.exports = [
   {
     message: 'App or Plugin?',
     choices: [
@@ -166,15 +166,3 @@ const questions = [
     }]
   }
 ];
-
-if (process.env.CI) {
-  // Add feature "ci" when running in CI environment.
-  questions.push({
-    message: '',
-    choices: [
-      {value: 'ci', title: 'Running in CI environment'}
-    ]
-  });
-}
-
-module.exports = questions;
