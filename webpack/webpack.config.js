1f3261df37022ec625e491349c017c8f301701b8
Avraham Essoudry <essoudry@gmail.com>
2020-09-17 01:29:58 +0300

    fix(webpack) removed unused variable

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -19,7 +19,6 @@ const when = (condition, config, negativeConfig) =>
 // primary config:
 const outDir = path.resolve(__dirname, project.platform.output);
 const srcDir = path.resolve(__dirname, 'src');
-const nodeModulesDir = path.resolve(__dirname, 'node_modules');
 const baseUrl = '';
 
 const cssRules = [
